8a3daff7b26b7d3aafdc5565a0c46adddd42c8ab
bs585@njit.edu <bs585@njit.edu>
2022-12-01 12:38:08 -0500

    saving...

---
+++ ChatRoom/common/Constants.java
@@ -0,0 +1,5 @@
+package ChatRoom.common;
+
+public abstract class Constants {
+    public static final long DEFAULT_CLIENT_ID = -1L;
+}
---
+++ ChatRoom/common/PayloadType.java
@@ -0,0 +1,6 @@
+//part 8 slides
+
+package ChatRoom.common;
+public enum PayloadType {
+    CONNECT, DISCONNECT, MESSAGE, CLIENT_ID, RESET_USER_LIST, SYNC_CLIENT, CREATE_ROOM, JOIN_ROOM, GET_ROOMS
+}
