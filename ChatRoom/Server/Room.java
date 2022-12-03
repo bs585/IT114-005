14c67cc21677d5ba90d57f7d7cf8da96fa313126
bs585@njit.edu <bs585@njit.edu>
2022-12-02 22:29:06 -0500

    project updated

--- ChatRoom/Server/Room.java
+++ ChatRoom/Server/Room.java
@@ -20,6 +20,8 @@ public class Room implements AutoCloseable {
 	private final static String DISCONNECT = "disconnect";
 	private final static String LOGOUT = "logout";
 	private final static String LOGOFF = "logoff";
+	private final static String FLIP = "flip";
+	private final static String ROLL = "roll";
 	private static Logger logger = Logger.getLogger(Room.class.getName());
 
 	public Room(String name) {
@@ -110,6 +112,38 @@ public class Room implements AutoCloseable {
 					case LOGOFF:
 						Room.disconnectClient(client, this);
 						break;
+
+					/*
+					 * BS585
+					 * 11/15/2022
+					 * What flip does is using a random number generator that picks a number between 1-2. Using an if statement
+					 * if the number is 1 the users gets "You get heads" on their screen
+					 * if the number is 2 the users get "you get tails".  sendMessage() sends the message to the users and 
+					 * prompts them the message. 
+					 */
+					case FLIP:
+						int flip = (int)((Math.random()*(2))+1);
+						String flipMessage = "You got heads!";
+						if(flip ==2){
+								flipMessage = "You got tails!";
+						}
+						sendMessage(client, flipMessage);
+						wasCommand = true;
+						break;
+				/*
+				 *BS585
+				 * 11/15/2022
+				 * What roll is doing is using math.random to generator a roll between 0-4.
+				 * once /roll is used, rollmessage is than printed out with the dice roll number. 
+				 * sendmessage is used to prompt the users the rollMessage, message.
+				 */
+					case ROLL:
+						int roll = (int)((Math.random()*(5)));
+						String rollMessage = "You rolled a "+Integer.toString(roll)+" (dice roll is 0-4)";
+						sendMessage(client, rollMessage);
+						wasCommand = true;
+						break;
+					
 					default:
 						wasCommand = false;
 						break;
